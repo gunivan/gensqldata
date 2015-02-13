13c1cbcaf5492cb19b1b7d996051e648b9cc6f6f
TrongLe <trongle@qasymphony.com>
2015-02-13 15:13:57 +0700

    update

--- PopulateSqlData/PopulateSqlData/Properties/AssemblyInfo.cs
+++ PopulateSqlData/PopulateSqlData/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PopulateSqlData")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HaVa Solution")]
 [assembly: AssemblyProduct("PopulateSqlData")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © HVS 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
